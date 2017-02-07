ee9ea468a211a3132019f136e0f878d6ca374b0c
Rafa Pagés <blinkmenow@gmail.com>
2017-02-07 11:03:31 +0000

    Updated license file and headers

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -1,15 +1,14 @@
 /* 
- *  Copyright (c) 2015-2016  Rafael Pagés (rps (at) gti.ssr.upm.es)
- *    and Universidad Politécnica de Madrid
+ *  Copyright (c) 2015-2017  Rafael Pagés (rafael.pages (at) scss.tcd.ie)
  *
- *  This file is part of Multitex
+ *  This file is part of SSMVtex
  *
- *  Multitex is free software; you can redistribute it and/or modify
+ *  SSMVtex is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
- *  Multitex is distributed in the hope that it will be useful,
+ *  SSMVtex is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
